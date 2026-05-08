f5016c8b95a6ff1d1afe8124b9076e0ab4437cbd
Cristian Castro <cristian.cespinoza@alumnos.upm.es>
2026-05-08 20:00:29 +0200

    Modulo de sensores piezoelectricos

---
+++ Proyecto ISE/PIEZOS_REACT/Objects/vl53l0x_api_calibration.d
@@ -0,0 +1,12 @@
+./objects/vl53l0x_api_calibration.o: Api\vl53l0x_api_calibration.c \
+  Api\vl53l0x_api.h Api\vl53l0x_api_strings.h Api\vl53l0x_def.h \
+  Api\vl53l0x_device.h Api\vl53l0x_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h Api\vl53l0x_platform.h \
+  Api\vl53l0x_platform_log.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  Api\vl53l0x_i2c_platform.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h Api\vl53l0x_api_core.h \
+  Api\vl53l0x_api_calibration.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h
